a57a0dacbd0adf8d398fbe9f2b471122d6d0e01c
Costanttan-9319 <258697556+costanttan-9319@users.noreply.github.com>
2026-02-05 10:32:10 +0800

    Fix D4 folder mapping

---
+++ Module1/LabExer/D4/Labex5.js
@@ -0,0 +1,15 @@
+// Array with 5 elements representing car brands
+let carBrands = ["Toyota", "Honda", "Ford", "Mercedes", "BMW"];
+
+//Replace the value of the element at position 1 and 4.
+carBrands[1] = "Hyundai";
+carBrands[4] = "Audi";
+
+//Add a new element to the beginning of the array
+carBrands.unshift("Chevrolet");
+
+//Remove the element at the end of the array
+carBrands.pop();
+
+//Print the array to the console.
+console.log(carBrands);
