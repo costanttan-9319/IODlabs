c75e7ffa6cbe54fa26c33eb1a9307d435daf8fb5
Costanttan-9319 <258697556+costanttan-9319@users.noreply.github.com>
2026-02-05 12:02:37 +0800

    Complete Lab Exercise 5: Cars Array

--- Module1/LabExer/D4/Labex5.js
+++ Module1/LabExer/D4/Labex5.js
@@ -2,8 +2,8 @@
 let carBrands = ["Toyota", "Honda", "Ford", "Mercedes", "BMW"];
 
 //Replace the value of the element at position 1 and 4.
-carBrands[1] = "Hyundai";
-carBrands[4] = "Audi";
+carBrands[0] = "Hyundai";
+carBrands[3] = "Audi";
 
 //Add a new element to the beginning of the array
 carBrands.unshift("Chevrolet");
---
+++ Module1/LabExer/D4/Labex6.js
@@ -0,0 +1,56 @@
+//Creating the book: title, description, author, num of pages
+let book = {
+    title: "The Great Wall",
+    description: "A documentation of the construction of The Great Wall of China", 
+    author : "John Smith",
+    numOfPages: 338
+};
+
+//printing object values in console individually (1) and via whole book object (2)
+//Printing objects property values individually (1)
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.numOfPages);
+
+
+//Printing whole book object
+console.log(book);
+
+//Update of book description
+book.description = "Update: A tale of the secret chambers of the Great Wall.";
+
+//Print updated book description
+console.log(book.description);
+
+
+//Extension: Create an array of 5 book objects
+let library = [
+    {title: "The Great Wall",
+    description: "A documentation of the construction of The Great Wall of China", 
+    author : "John Smith",
+    numOfPages: 338},
+
+    {title: "The 3 little pigs",
+    description: "A classical bedtime story", 
+    author : "Candy Lim",
+    numOfPages: 200},
+
+    {title: "The 3 little pigs",
+    description: "A classical bedtime story", 
+    author : "Candy Lim",
+    numOfPages: 200},
+
+    {title: "Harry Potter",
+    description: "Journey of a young wizard who becomes the best", 
+    author : "Henry Goth",
+    numOfPages: 1023},
+
+    {title: "The Apple Store",
+    description: "Biography of how Steve Jobs founded Apple", 
+    author : "James Watson",
+    numOfPages: 678}
+];
+
+//Print arrays
+console.log(library);
