6f4bfac1986e574440094d157301288aec897dc1
Costanttan-9319 <258697556+costanttan-9319@users.noreply.github.com>
2026-02-26 10:22:44 +0800

    updatejsadvance

--- Module3/labExer/labex3.1/JSAdvance.js
+++ Module3/labExer/labex3.1/JSAdvance.js
@@ -258,5 +258,50 @@ function multiply(a, b, c, d) {
 
 multiply.delay(500)(5, 5); // prints 25 after 500 milliseconds. part c is multiply.delay(500)(5, 5, 4, 5); 
 
+/*
+7. The following DigitalClock class uses an interval to print the time every second once
+started, until stopped.
 
+a) Create a new class PrecisionClock that inherits from DigitalClock and adds the
+parameter precision – the number of ms between 'ticks'. This precision parameter
+should default to 1 second if not supplied.
+*/
 
+class DigitalClock {
+constructor(prefix) {
+this.prefix = prefix;
+}
+display() {
+let date = new Date();
+//create 3 variables in one go using array destructuring
+let [hours, mins, secs] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+
+if (hours < 10) hours = '0' + hours;
+if (mins < 10) mins = '0' + mins;
+if (secs < 10) secs = '0' + secs;
+console.log(`${this.prefix} ${hours}:${mins}:${secs}`);
+}
+stop() {
+clearInterval(this.timer);
+}
+start() {
+this.display();
+this.timer = setInterval(() => this.display(), 1000);
+}
+}
+
+//Create a new class PrecisionClock that inherits from DigitalClock
+class PrecisionClock extends DigitalClock {
+    constructor(prefix, precision = 1000) {
+        super(prefix)
+        this.precision = precision;
+    }
+
+    start() {
+        this.display();
+        this.timer = setInterval(() => this.display(), this.precision);
+    }
+}
+
+const myPrecisionClock = new PrecisionClock('my precision clock:', 500);
+myPrecisionClock.start();
