746831b1adbeaf4d4b1172ae4869c08c86c6a00f
Costanttan-9319 <258697556+costanttan-9319@users.noreply.github.com>
2026-03-13 13:07:44 +0800

    update

--- Module5/Labexer/Labex5.1/5.1server1.js
+++ Module5/Labexer/Labex5.1/5.1server1.js
@@ -1,17 +1,38 @@
 const express = require("express");
 
-const app = express();
+//=== Server 1 configuration ====
+const app1 = express();
+const port1 = 3005;
 
-const port = 3000;
-
-app.get('/', (req, res) => {
+app1.get('/', (req, res) => {
     res.send("I am server 1!");
 });
 
-app.get("/test", (req, res) => {
+app1.get("/test", (req, res) => {
     res.send("Test for server 1 good!");
-})
+});
 
-app.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`);
+
+//=== Server 2 configuration ===
+const app2 = express();
+const port2 = 3014;
+
+app2.get('/', (req, res) => {
+    res.send("I am server 2!");
+});
+
+app2.get("/test", (req, res) => {
+    res.send("Test for server 2 good!");
+});
+
+
+
+//====Message of servers running====
+
+app1.listen(port1, () => {
+    console.log(`Running at 🛰️ http://localhost:${port1}`);
+});
+
+app2.listen(port2, () => {
+    console.log(`Running at 🛰️ http://localhost:${port2}`);
 });
